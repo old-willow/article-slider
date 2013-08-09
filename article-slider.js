0aaad22035b7f393c988459bb9703d6c45b27f9c
Robert Kolozsi <robi@midearth.elda>
2013-08-09 12:17:54 +0200

    A lot of thigs. This branch is consider broken from now.

--- article-slider.js
+++ article-slider.js
@@ -2,103 +2,239 @@
  * file: article-slider.js
  * maintainer: Kolozsi Róbert <kolozsi.robert@gmail.com>
  * date: Nov 13, 2012
- * last update: March 21, 2013
+ * last milestone: March 21, 2013
+ * last update: Aug 8, 2013
  */
 
 /* This is a prototype project. With time it will probably evolve in to object API.*/
+
+/* index.html is the basic html setup for this slider. */
 
 
 (function(window, undefined) {
     var document = window.document;
     // Container div element (static in page.)
-    var container = document.getElementById('container');
+    var container = document.getElementById('main_container');
     var scroll_div = document.getElementById('scroll_div');
     var art_item;
     var container_height;
-    var container_width = container.offsetWidth - 2;  // 2 - stands for 2px for the border of the container.
+    var container_width;
     var scroll_div_width;
     var scroll_div_height;
 
     /* Set this either for horizontal either for vertical orientation of article slider. */
-    var orientation = {
-        horizontal: true,
-        vertical: false
-    };
+    //var orientation = {
+    //    horizontal: true,
+    //    vertical: false
+    //};
 
-    /* Just for checking the orientation setting above. */
-    if (!orientation.horizontal && !orientation.vertical) || (orientation.horizontal && orientation.vertical) {
-        throw {
-            name: "Error: Wrong orientation settings!",
-            message: "Can't have horizontal and vertical setting the same value!"
-        }
-        console.log("Error: Wrong orientation settings!");
+    //var ORIENT = {
+    //    horizontal: 1,
+    //    vertical: 0
+    //};
+
+    /* ASOB === Article Slider Object */
+    var ASOB = {
+        /* Orientation. */
+        /* Default setting is a horizontal orientation. */
+        orientation: {
+            horizontal: 1,
+            vertical: 0
+        },
+        var getOrientation: function() {
+            return this.orientation;
+        },
+
+        //var setOrientation: function(o) {
+        //    this.orientation = o;
+        //},
+
+        numberOfRows: 1,  // Default.
+        numberOfColumns: 1,  // Default.
+        var setNumberOfColumns: function(n) {
+            (function() {
+                if (this.orientation.horizontal && !this.orientation.vertical) {
+                    this.numberOfColumns = parseInt(Math.round(this.numberOfArticles/ this.numberOfRows));
+                } else if (this.orientation.vertical && !this.orientation.horizontal) {
+                    //
+                } else {
+                    console.log("ERROR: Can't have both orientations true or false at the same time.");
+                }
+            })();
+        },
+
+        /* Space between two articles (both: horiz, vert) and space between article and scroll_div. */
+        gap: 10,
+
+        /* Main Container. (the main container div) */
+        mainContainer: null,
+        var setMainContainer: function() {
+            this.mainContainer = document.getElementById('main_container');
+            if (this.mainContainer) {
+                this.setNumberOfColumns();
+                this.mainContainer.style.width =  this.mainContainerWidth + 'px';
+            } else {
+                console.log("ERROR: You didn't named id properly your main container or you don't have any main container element in html file.");
+            }
+        },
+        var getMainContainer: function() {
+            return this.mainContainer;
+        },
+        mainContainerHeight: 0,
+        mainContainerWidth: 0,
+        var setMainContainerWidth = function() {
+            /*
+             * Width of mainContainer is depend on these parameters:
+             * numberOfColumns, articleItemWidth and gap. */
+            this.mainContainerWidht = this.numberOfColums * this.articleItemWidth + (2 * this.gap);
+
+        },
+
+        // Some css setting form article div.
+        articleItemWidth: 150,
+        articleItemHeight: 100,
+
+        scrollContainer: null,
+        var setScrollContainer: function() {
+            this.scrollContainer = document.getElementById('scroll_div');
+        },
+        var getScrollContainer: function() {
+            return this.scrollContainer;
+        },
+        scrollContainerWidth: '',
+        scrollContainerHeight: '',
+
+        numberOfArticles: 84,
+
+
+    /*
+     * Navigation buttons or navigation divs are in function of width and height of ASOB.mainContainer.
+     * If the number of articles mach the width and height of ASOB.mainContainer, don't excide out of boundaries,
+     * then navigations are not needed. In case the number of articles can't fit in ASOB.mainContainer width and height
+     * then navigation divs are necessery and are created.
+    */
+        navigatorUpLeft: null,
+        navigatorDownRight: null,
     }
 
-    // Navigation placeholders. Used if the number of articles excide 18.
-    var left_nav;
-    var right_nav;
+    /* Just for checking the orientation setting above. */
+    //if ((!orientation.horizontal && !orientation.vertical) || (orientation.horizontal && orientation.vertical)) {
+    //    throw {
+    //        name: "Error: Wrong orientation settings!",
+    //        message: "Can't have horizontal and vertical setting the same value!"
+    //    }
+    //    console.log("Error: Wrong orientation settings!");
+    //}
 
-    var my_top;
-    var my_left;  // For positioning article divs.
+    // Navigation placeholders.
+    // Used if the number of articles excide 18 in horizontal orientation,
+    // or if in vertical orientation excide 6.
+    //var left_nav;
+    //var right_nav;
 
-    /* Basic manual setup of articles.
+    //var up_nav;
+    //var down_nav;
+
+    //var my_top;
+    //var my_left;  // For positioning article divs.
+
+    /*
+     * Basic manual setup of articles.
      * This value will be acquired dinamically by querying database.
      */
-    var NUMBER_OF_ARTICLES = 84;
+    //var NUMBER_OF_ARTICLES = 84;
+    //var GAP = 10;  // Should be changeable with a desing of page.
+    //var article_item_width = 150; // Should be changeable with a desing of page.
+    //var article_item_height = 100; // Should be changeable with a desing of page.
 
-    if (orientation.horizontal) {
-        var NUMBER_OF_ROWS = 3;
-        var NUMBER_OF_COLUMNS = parseInt(Math.round(NUMBER_OF_ARTICLES / NUMBER_OF_ROWS));
+    /* Setting the mainContainer object. */
+    ASOB.setMainContainer();
 
-        /* Check if number of columns correspond to the number of articles.
-        * If not then adjust by adding one more column.
-        */
+    if (ASOB.horizontal && !ASOB.vertical) {
+        //var NUMBER_OF_ROWS = 3;
+        //var NUMBER_OF_COLUMNS = parseInt(Math.round(NUMBER_OF_ARTICLES / NUMBER_OF_ROWS));
+        //container_width = container.offsetWidth - 2;  // 2 - stands for 2px for the border of the container.
+        //container_width = (article_item_width * NUMBER_OF_COLUMNS) + (GAP * (NUMBER_OF_COLUMNS + 1));
+        //container.style.width = 900 + 'px';
+        container_width = container.offsetWidth - 2;  // 2 - stands for 2px for the border of the container.
+        //container.style.width = (container_width - 2) + "px";  // 2 - stands for 2px for the border of the container.
+
+        /*
+         * Check if number of columns correspond to the number of articles.
+         * If not then adjust by adding one more column.
+         */
         var reminder = NUMBER_OF_ARTICLES % NUMBER_OF_ROWS;
-        console.log("reminder: " + reminder);
+        //console.log("reminder: " + reminder);
 
         if (reminder === 1) {
             NUMBER_OF_COLUMNS += 1;
         }
 
-        console.log("NUMBER_OF_ARTICLES: " + NUMBER_OF_ARTICLES);
-        console.log("NUMBER_OF_COLUMNS: " + NUMBER_OF_COLUMNS);
+        //console.log("NUMBER_OF_ARTICLES: " + NUMBER_OF_ARTICLES);
+        //console.log("NUMBER_OF_COLUMNS: " + NUMBER_OF_COLUMNS);
 
-    } else if (orientation.vertical) {
+    } else if (ASOB.vertical && !ASOB.horizontal) {
+        //container_width = container.offsetWidth - 2;  // 2 - stands for 2px for the border of the container.
+        container_width = article_item_width + (2 * GAP);
+        container.style.width = container_width + "px";
         var NUMBER_OF_ROWS = 6;
+        var NUMBER_OF_COLUMNS = 1;
+
     }
 
-    var GAP = 10;  // Change with a desing of page.
-
-    var art_item_width = 150; // Change with a desing of page.
-    var art_item_height = 100; // Change with a desing of page.
-
-    // Calculate main container height.
-    container_height = (NUMBER_OF_ROWS * art_item_height) + ((NUMBER_OF_ROWS + 3) * GAP);
+    // Calculate main container height. I is same for vertical and horizontal orientations.
+    container_height = (NUMBER_OF_ROWS * article_item_height) + ((NUMBER_OF_ROWS + 3) * GAP);
+    //console.log("NUMBER_OF_ROWS + 3: " + NUMBER_OF_ROWS + 3);
     container.style.height = container_height + 'px';
 
     // Calculate scroll_div width & height.
-    scroll_div_width = (NUMBER_OF_COLUMNS * art_item_width) + ((NUMBER_OF_COLUMNS + 1) * GAP);
-    scroll_div_height = (NUMBER_OF_ROWS * art_item_height) + ((NUMBER_OF_ROWS + 1) * GAP);
-    scroll_div.style.width = scroll_div_width + 'px';
-    scroll_div.style.height = scroll_div_height + 'px';
-    console.log("scroll_div_width: " + scroll_div_width);
-    var scroll_div_max_movement = scroll_div_width - container_width;
-    console.log("scroll_div_max_movement: " + scroll_div_max_movement);
+    if (orientation.horizontal) {
+        scroll_div_width = (NUMBER_OF_COLUMNS * article_item_width) + ((NUMBER_OF_COLUMNS + 1) * GAP);
+        scroll_div_height = (NUMBER_OF_ROWS * article_item_height) + ((NUMBER_OF_ROWS + 1) * GAP);
+        scroll_div.style.width = scroll_div_width + 'px';
+        scroll_div.style.height = scroll_div_height + 'px';
+        console.log("scroll_div_width: " + scroll_div_width);
+        var scroll_div_max_movement = scroll_div_width - container_width;
+        console.log("scroll_div_max_movement: " + scroll_div_max_movement);
 
-    // Partitioning scroll_div_max_movement in to equal parts, according to max one step movement.
-    // max one step movement is currently (2xNUMBER_OF_COLUMNS) + (2xGAP).
-    var move_limit = 2 * art_item_width + 2 * GAP; // 170px
-    var equal_divisor = parseInt(Math.floor(scroll_div_max_movement / move_limit));
-    console.log("equal divisor: " + equal_divisor);
-    console.log("move limit: " + move_limit);
-    console.log("equal divisor: " + equal_divisor);
-    move_limit = scroll_div_max_movement / equal_divisor;
-    console.log("<recalculated> move limit: " + move_limit);
+        // Partitioning scroll_div_max_movement in to equal parts, according to max one step movement.
+        // max one step movement is currently (2xNUMBER_OF_COLUMNS) + (2xGAP).
+        var move_limit = 2 * article_item_width + 2 * GAP; // 170px
+        var equal_divisor = parseInt(Math.floor(scroll_div_max_movement / move_limit));
+        console.log("equal divisor: " + equal_divisor);
+        console.log("move limit: " + move_limit);
+        console.log("equal divisor: " + equal_divisor);
+        move_limit = scroll_div_max_movement / equal_divisor;
+        console.log("<recalculated> move limit: " + move_limit);
 
-    var SAVED_MOVE_LIMIT = move_limit;
-    var scroll_reminder = 0;
+        var SAVED_MOVE_LIMIT = move_limit;
+        var scroll_reminder = 0;
 
-    container.style.overflowX = 'hidden';
+        container.style.overflowX = 'hidden';
+    } else if (orientation.vertical) {
+        scroll_div_width = (NUMBER_OF_COLUMNS * article_item_width) + ((NUMBER_OF_COLUMNS + 1) * GAP);
+        scroll_div_height = (NUMBER_OF_ROWS * article_item_height) + ((NUMBER_OF_ROWS + 1) * GAP);
+        scroll_div.style.width = scroll_div_width + 'px';
+        scroll_div.style.height = scroll_div_height + 'px';
+        console.log("scroll_div_width: " + scroll_div_width);
+        var scroll_div_max_movement = scroll_div_width - container_width;
+        console.log("scroll_div_max_movement: " + scroll_div_max_movement);
+
+        // Partitioning scroll_div_max_movement in to equal parts, according to max one step movement.
+        // max one step movement is currently (2xNUMBER_OF_COLUMNS) + (2xGAP).
+        var move_limit = 2 * article_item_width + 2 * GAP; // 170px
+        var equal_divisor = parseInt(Math.floor(scroll_div_max_movement / move_limit));
+        console.log("equal divisor: " + equal_divisor);
+        console.log("move limit: " + move_limit);
+        console.log("equal divisor: " + equal_divisor);
+        move_limit = scroll_div_max_movement / equal_divisor;
+        console.log("<recalculated> move limit: " + move_limit);
+
+        var SAVED_MOVE_LIMIT = move_limit;
+        var scroll_reminder = 0;
+
+        container.style.overflowX = 'hidden';
+    }
 
 
     /* ********************************
@@ -109,7 +245,7 @@
     for (i = 0; i < NUMBER_OF_COLUMNS; i += 1) {
         my_top = GAP / 2;
         for (j = 0; j < NUMBER_OF_ROWS; j += 1) {
-            if (art_counter <= NUMBER_OF_ARTICLES) {  // Don't draw the none existing article boxs.
+            if (art_counter <= NUMBER_OF_ARTICLES) {  // Don't draw the none existing article boxes.
                 art_item = document.createElement('div');
                 scroll_div.appendChild(art_item);
                 art_item.setAttribute('class', 'article_item');
@@ -119,10 +255,10 @@
                 art_item.style.top = my_top + "px";
                 art_item.style.left = my_left + "px";
 
-                my_top += art_item_height + GAP;
+                my_top += article_item_height + GAP;
             }
         }
-        my_left += art_item_width + GAP;
+        my_left += article_item_width + GAP;
     }
 
 
@@ -378,9 +514,11 @@
         };
 
         var slider_mouseup = function(e) {
-            /* This function is only executed if there was a mousedown event on
-             * slider_div element and ti turned off the: up = false.
-             * It is attached to the document element. */
+            /*
+             * This function is only executed if there was a mousedown event on
+             * slider_div element and it turned off the: up = false.
+             * It is attached to the document element.
+             * */
             if (!e) {
                 var e = window.event;
             }
